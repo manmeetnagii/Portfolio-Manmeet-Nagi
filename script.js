7c64b8c711454a2cde52cd7f64a1a81ca8c73786
Manmeet <manmeetnagiii@gmail.com>
2023-11-04 14:43:07 +0530

    added

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ const hamburger = document.querySelector('.material-symbols-outlined');
 const navlist = document.querySelector('.navbar-links--container');
 
 hamburger.addEventListener('click', () => {
-    navlist.classList.toggle('.show');
+    navlist.classList.toggle('show');
+});   
+
+
+document.querySelectorAll('a[href^="#').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
 });
